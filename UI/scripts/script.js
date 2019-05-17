df8a335d23a4765c37f4e48c815649c805adc13c
Lafiagi <skillfullmallam@gmail.com>
2019-05-17 22:24:24 +0100

    Adding pages

--- UI/scripts/script.js
+++ UI/scripts/script.js
@@ -2,3 +2,7 @@ document.querySelector("#btn").addEventListener("click", function(event) {
 	//alert(1)
          event.preventDefault();
 }, false);
+
+function home() {
+	window.location.href="index.html"
+}

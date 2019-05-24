2a26fef2b150e5a46c4df8760f07d29d1b317027
Lafiagi <skillfullmallam@gmail.com>
2019-05-24 08:59:17 +0100

    feature(sold) : completed the UI of the mark as sold page [Finishes #165952645]

--- UI/scripts/script.js
+++ UI/scripts/script.js
@@ -2,3 +2,11 @@ document.querySelector("#btn").addEventListener("click", function(event) {
 	//alert(1)
          event.preventDefault();
 }, false);
+
+function handleClick(value) {
+   
+   if (value) {
+   	document.querySelector(".updatesold").style.visibility = "visible"
+   }
+   else document.querySelector(".updatesold").style.visibility = "hidden"
+}
